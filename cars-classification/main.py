aacbc3de50540d64bd1d52323a932099cc668872
Henrique Dal Bello Batista <henrique.dalbello@t-systems.com.br>
2020-12-24 14:08:11 -0300

    predictions test

--- cars-classification/main.py
+++ cars-classification/main.py
@@ -21,41 +21,38 @@ lug_capacity = pd.get_dummies(cars.lug_capacity, prefix='lug_capacity')
 safety = pd.get_dummies(cars.safety, prefix='safety')
 labels = pd.get_dummies(cars.output, prefix='condition')
 
+print(price)
 x = pd.concat([price, maint, doors, persons, lug_capacity, safety], axis=1)
 y = labels.values
-#print(x)
+print(x)
 #print(y)
 input_layer = Input(shape=(x.shape[1],))
 dense_layer_1 = Dense(15, activation='relu')(input_layer)
 dense_layer_2 = Dense(10, activation='relu')(dense_layer_1)
 output = Dense(y.shape[1], activation='softmax')(dense_layer_2)
 
-#print(input_layer)
-#print(dense_layer_1)
-#print(dense_layer_2)
-#print(output)
-
 model = Model(inputs=input_layer, outputs=output)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
-history = model.fit(x_train, y_train, batch_size=8, epochs=50, verbose=1, validation_split=0.2)
+history = model.fit(x_train, y_train, batch_size=8, epochs=50, verbose=0, validation_split=0.2)
 
-score = model.evaluate(x_test, y_test, verbose=1)
+score = model.evaluate(x_test, y_test, verbose=0)
 print(history)
 print("Score:", score[0])
 print("Accuracy:", score[1])
 
+test = x_test.iloc[:21]
 
-predictions = model.predict(x_test)
+predictions = model.predict(test)
+index = np.argmax(predictions[0])
+#print(cols[index])
+#print(labels)
+#print(labels.values[index])
+#print(doors)
+print(test)
+print(test.columns[np.argmax(labels.values[index])])
 print(predictions)
-print('Test:')
-print(x_test[0])
-print('Prediction:')
-print(predictions[0])
-#sc = StandardScaler()
-#x_train = sc.fit_transform(x_train)
-#y_train = sc.fit_transform(y_train)
-
+#print(predictions)
 #print(model.summary())
